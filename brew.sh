852f9fb636ff10838670da393073cebab30d12e9
Luke Giuliani <luke@giuliani.com.au>
2018-05-02 13:16:17 +1000

    updated paths based on advised command

--- brew.sh
+++ brew.sh
@@ -14,7 +14,7 @@ then
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
